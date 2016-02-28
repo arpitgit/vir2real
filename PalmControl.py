230f612f51f90f2816abdf875c2d827b5a66bbc4
Arpit Goyal <arpitg@vt.edu>
2016-02-27 21:12:53 -0500

    New Unity client

--- PalmControl.py
+++ PalmControl.py
@@ -17,9 +17,6 @@ class PalmControlListener(Leap.Listener):
 	def on_disconnect(self, controller):
 		print "Disconnected"
 
-	def on_disconnect(self, controller):
-		print "Disconnected"
-
 	def on_frame(self, controller):
 		frame = controller.frame()
 		if not frame.hands.is_empty:
@@ -33,7 +30,7 @@ class PalmControlListener(Leap.Listener):
 				self.two_hand_gesture_recognition(left_hand, right_hand)
 
 	def one_hand_gesture_recognition(self, hand):
-		if hand.grab_strength < 1:
+		if hand.grab_strength < 0.5:
 			normal = hand.palm_normal
 			velocity = hand.palm_velocity
 			cmd_dict = {'action': 'explore', 'normal_x': normal.x, 'normal_y': normal.y, 'normal_z': normal.z, 'velocity_x': velocity.x, 'velocity_y': velocity.y, 'velocity_z': velocity.z}
